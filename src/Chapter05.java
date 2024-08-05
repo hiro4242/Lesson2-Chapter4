6152b342527d49226b99b33b20df837e02c3ebb3
Hirotaka <36596960+hiro4242@users.noreply.github.com>
2024-08-05 20:45:42 +0900

    test2

---
+++ src/Chapter05.java
@@ -0,0 +1,13 @@
+
+public class Chapter05 {
+
+	public static void main(String[] args) {
+		String myname = "taro";
+		System.out.println(myname);
+		myname = "takumi";
+		final int LUCKY = 7; 
+		System.out.println(LUCKY);
+
+	}
+
+}
